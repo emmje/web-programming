b03c22a15ff33ac8a3fbccf42adb1b1e222a65f6
Nicole Mary Johnson <145398267+emmje@users.noreply.github.com>
2025-03-06 19:49:10 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const modeToggle = document.getElementById('mode-toggle');
-const body = document.body;
-
-modeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-
-    // Toggle button icon based on mode
-    modeToggle.textContent = body.classList.contains('dark-mode') ? '🌙' : '☀️';
-});
